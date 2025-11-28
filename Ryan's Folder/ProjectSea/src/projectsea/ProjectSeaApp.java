1a1c22c3680f838a9701c591fb7db2463fc5afb4
ryanl24 <148884215+ryanl24@users.noreply.github.com>
2025-11-28 23:16:54 +0000

    project nearly done have all guis set up just doing some finsihing touches

--- Ryan's Folder/ProjectSea/src/projectsea/ProjectSeaApp.java
+++ Ryan's Folder/ProjectSea/src/projectsea/ProjectSeaApp.java
@@ -2,19 +2,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package seasaver;
+package projectsea;
 
 /**
  *
  * @author ryanl
  */
-public class SeaSaverApp {
+public class ProjectSeaApp {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        ProjectGUI mainGUI = new ProjectGUI();
+        mainGUI.setVisible(true);
         // TODO code application logic here
+        
     }
     
 }
